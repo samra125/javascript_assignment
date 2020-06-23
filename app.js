9de1d6025bcd85afef3127a84b5d30b11b0a40d7
samraansari <samraansari27@gmail.com>
2020-06-23 18:25:14 +0500

    javascript chapter 1 to 20 assignment

---
+++ app.js
@@ -0,0 +1,151 @@
+// ..................................................CHAPTER 1
+// // TASK1
+
+//  alert("WELCOME TO MY WEBSITE");
+//  // TASK2
+
+// alert("please! enter a vali password ");
+
+// // TASK3
+// alert("welcome to js land  happy coding");
+
+// // TASK4
+// alert("welcome to js land "  + '\n' +"happv coding")
+
+
+// //TASK 5
+// console.log("hello! i can run JS from my webrowser console");
+
+//................................................. CHAPTER 2
+// // TASK1
+// var username ="cc";
+
+// //TASK2
+// var myName= "samra";
+
+// //TASK3
+// var message = "hello world"
+// alert(message);
+
+// //task4
+// var studentsname= "jhon"
+// alert(studentsname);
+// var age = 15
+// alert(age);
+// var degree= "web and mobile development"
+// alert (degree)
+
+// //task 5
+// var z = ( "PIZZA" + '\n' +  
+// "PIZZ" + '\n' +  
+//  "PIZ"  + '\n' + "PI" + '\n' + "P" + '\n');  
+
+
+// alert( z);
+
+//task 6
+//var email = "my email address is examplw@example.com"
+//alert(email)
+
+//task7
+// var book = "i am trying to learn from the book a smarter way to learn javascript"
+// alert(book)
+
+//,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,chapter 3
+
+//task 1
+
+// var age= " i am 15 years old"
+// alert(age)
+
+// // task2
+
+// var visit = "You have visited this site N times"
+// alert(visit)
+
+
+//task 3
+
+//var birthYear = 1997
+//console.log ("my birth year is" + birthYear  + '/n' + "data type is in numbered")
+
+//task4
+
+// var VisitorName = "John Doe"
+//  var Product = " T-shirt(s)"
+//  var title = "clothing house" 
+//  var  Quantity ="5"
+//  console.log( "hi "+VisitorName +" ordered " + Quantity +Product + " on " +title);
+
+ //,,,,,,,,,,,,,,,,,,,,,,,,,,,,,chapter4
+
+ //task1
+//  var name = c;
+
+ //task2
+
+ //legal variable 
+//  var name = 13
+//  var myNamevar = s
+//  var $myname = "cat"
+//  var myname23 = 22
+//  var name_my = 22
+
+//  //illegal
+//  var 2name =22
+//  var "myNamevar" = s
+//  var my name = s
+//  var alert = c
+
+//  //task3
+//  alert(" A heading stating “Rules for naming JS variables")
+// alert (" Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable")
+// alert ("Variables must begin with a ______, ______ or _____. For example $name, _name or name")
+// alert (" Variable names are case sensitive")
+// alert ("Variable names should not be JS keyword")
+
+
+//.....................................chapter5
+
+//task 1
+// var a= 3;
+// var b = 5;
+// var c= a+b;
+// alert("sum of " +a + "and" +b + "is" +c);
+
+// //task 2
+// var a= 3;
+// var b = 5;
+// var c= a-b;
+// alert("subtraction of " +a + "and" +b + "is" +c);
+
+// var a= 3;
+// var b = 5;
+// var c= a*b;
+// alert("multiplication of " +a + "and" +b + "is" +c);
+
+// var a= 3;
+// var b = 5;
+// var c= a/b;
+// alert("divisio of " +a + "and" +b + "is" +c);
+
+
+
+//task3
+
+// var a = 5
+// console.log("value initialize is " +a)
+// console.log ("increment value is " + ++a)
+// var b =7
+// var c =a+b
+// console.log ("value aftr addition is"+ c)
+// console.log("increment is" + ++c)
+// console.log("value after decrement is " + --c)
+
+
+//task 4
+// var a =600
+// b= a*5
+// console.log("price of buying 5 ticket is " +b)
+
+//task5
